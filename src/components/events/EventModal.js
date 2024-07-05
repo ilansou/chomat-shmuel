c91cf3b218644fe1a1585ba8a40405169e1acc1f
Ilan <ilanshushan13@gmail.com>
2024-07-05 13:55:01 +0300

    files organization

--- src/components/events/EventModal.js
+++ src/components/events/EventModal.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 import { deleteDoc, doc } from "firebase/firestore";
-import { db } from "../firebase";
+import { db } from "../../firebase";
 
 export const EventModal = ({ event, onClose, onDelete }) => {
   const { user } = useAuth();
